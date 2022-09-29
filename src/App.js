6a01f502cc0bc63cba20f7dc695d9779c770e6db
LuscaGolD <110109440+LuscaGolD@users.noreply.github.com>
2022-09-29 08:25:49 -0300

    Base React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import AnotherComponent from './components/AnotherComponent';
+
+import FirstComponent from './components/FirstComponent';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
